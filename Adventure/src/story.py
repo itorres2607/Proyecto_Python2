6e01eb1343a5b807bcdf93f912348c7d46b9ddcd
Isaac Torres <ismatorr@gmail.com>
2013-01-04 11:28:34 -0500

    implementando metodos de cambio de pagina

--- Adventure/src/story.py
+++ Adventure/src/story.py
@@ -9,5 +9,16 @@ def evento_tecla(keys):
     if keys[K_q]:
         sys.exit(0)
 
+
+def avanza_pagina(pagina):
+    pag = "parte"+pagina
+    #ejecutar sonido de la parte que corresponde
+
+
+
+def retrocede_pagina(pagina):
+    pag = "parte"+pagina
+    #ejecuto sonido de la parte correspondiente
+
 if __name__ == "__main__":
     print "Hello World"
