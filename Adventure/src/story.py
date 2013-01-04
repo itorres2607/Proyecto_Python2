4b12324581abac935e6852a49c71002f183110dc
Isaac Torres <ismatorr@gmail.com>
2013-01-04 10:15:51 -0500

    Commit inicial del segundo proyecto de python, ya que como no hay nada especificado hay que preveerlo todo

---
+++ Adventure/src/story.py
@@ -0,0 +1,8 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="Isaac"
+__date__ ="$04-ene-2013 10:13:44$"
+
+if __name__ == "__main__":
+    print "Hello World"
