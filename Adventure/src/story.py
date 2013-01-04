cf738db93d8e95b88f4be595d46718a1f8c98e3b
Isaac Torres <ismatorr@gmail.com>
2013-01-04 16:11:25 -0500

    depurando errores con el formato mp3

--- Adventure/src/story.py
+++ Adventure/src/story.py
@@ -9,15 +9,15 @@ from pygame.locals import *
 
 def reproduce_parte(p_ant , keys):
     if keys[K_a] or keys[K_A]:
-        r = p_ant+"_1.mp3"
+        r = "grabaciones/"+p_ant+"_1.mp3"
         aux = p_ant+"_1"
 
     if keys[K_b] or keys[K_B]:
-        r = p_ant+"_2.mp3"
+        r = "grabaciones/"+p_ant+"_2.mp3"
         aux = p_ant+"_2"
         
     if keys[K_c] or keys[K_C]:
-        r = p_ant+"_3.mp3"
+        r = "grabaciones/"+p_ant+"_3.mp3"
         aux = p_ant+"_3"
 
     pygame.mixer.init()
@@ -36,13 +36,13 @@ def reproduce_parte(p_ant , keys):
     
 def reproduce_opcion(o_ant , keys):
     if keys[K_a] or keys[K_A]:
-        o = o_ant+"_1.mp3"
+        o = "opciones/"+o_ant+"_1.mp3"
         aux = o_ant+"_1"
     if keys[K_b] or keys[K_B]:
-        o = o_ant+"_2.mp3"
+        o = "opciones/"+o_ant+"_2.mp3"
         aux = o_ant+"_2"
     if keys[K_c] or keys[K_C]:
-        o = o_ant+"_3.mp3"
+        o = "opciones/"+o_ant+"_3.mp3"
         aux = o_ant+"_3"
 
     pygame.mixer.init()
@@ -57,12 +57,32 @@ def reproduce_opcion(o_ant , keys):
 
 
 
+def reproduccion_parte_inicial():
+    pygame.mixer.init()
+    pygame.mixer.music.load("grabaciones/parte1.mp3")
+    pygame.mixer.music.play()
+
+
+
+def reproduccion_opcion_inicial():
+    pygame.mixer.init()
+    pygame.mixer.music.load("opciones/opcion1.mp3")
+    pygame.mixer.music.play()
+
+
+
 def main():
+
+    reproduccion_parte_inicial()
+    reproduccion_opcion_inicial()
+    parte_anterior = "parte1"
+    opcion_anterior= "opcion1"
+
 
     while True:
         keys = pygame.key.get_pressed()
-        parte_anterior = reproduce_parte(parte_anterior , keys)
         opcion_anterior = reproduce_opcion(opcion_anterior , keys)
+        parte_anterior = reproduce_parte(parte_anterior , keys)
 
         if parte_anterior == "error" or opcion_anterior == "error":
             sys.exit(0)
