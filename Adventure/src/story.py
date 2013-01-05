ca90536cbdd8e948dafa32aa81fd854c8979f61b
Isaac Torres <ismatorr@gmail.com>
2013-01-05 09:36:20 -0500

    corrigiendo errores al usar el loop de reproduccion de voz

--- Adventure/src/story.py
+++ Adventure/src/story.py
@@ -8,16 +8,16 @@ from pygame.locals import *
 
 
 def reproduce_parte(p_ant , keys):
-    if keys[K_a] or keys[K_A]:
-        r = "grabaciones/"+p_ant+"_1.mp3"
+    if keys[K_a]:
+        r = "grabaciones/"+p_ant+"_1.ogg"
         aux = p_ant+"_1"
 
-    if keys[K_b] or keys[K_B]:
-        r = "grabaciones/"+p_ant+"_2.mp3"
+    if keys[K_b]:
+        r = "grabaciones/"+p_ant+"_2.ogg"
         aux = p_ant+"_2"
         
-    if keys[K_c] or keys[K_C]:
-        r = "grabaciones/"+p_ant+"_3.mp3"
+    if keys[K_c]:
+        r = "grabaciones/"+p_ant+"_3.ogg"
         aux = p_ant+"_3"
 
     pygame.mixer.init()
@@ -34,58 +34,63 @@ def reproduce_parte(p_ant , keys):
 
 
     
-def reproduce_opcion(o_ant , keys):
-    if keys[K_a] or keys[K_A]:
-        o = "opciones/"+o_ant+"_1.mp3"
-        aux = o_ant+"_1"
-    if keys[K_b] or keys[K_B]:
-        o = "opciones/"+o_ant+"_2.mp3"
-        aux = o_ant+"_2"
-    if keys[K_c] or keys[K_C]:
-        o = "opciones/"+o_ant+"_3.mp3"
-        aux = o_ant+"_3"
+#def reproduce_opcion(o_ant , keys):
+#    if keys[K_a] or keys[K_A]:
+#        o = "opciones/"+o_ant+"_1.mp3"
+#        aux = o_ant+"_1"
+#    if keys[K_b] or keys[K_B]:
+#        o = "opciones/"+o_ant+"_2.mp3"
+#        aux = o_ant+"_2"
+#    if keys[K_c] or keys[K_C]:
+#        o = "opciones/"+o_ant+"_3.mp3"
+#        aux = o_ant+"_3"
 
-    pygame.mixer.init()
-    try:
-        pygame.mixer.music.load(o)
-        pygame.mixer.music.play()
-    except:
-        aux = "error"
+#    pygame.mixer.init()
+#    try:
+#        pygame.mixer.music.load(o)
+#        pygame.mixer.music.play()
+#    except:
+#        aux = "error"
 
 
-    return aux
+#    return aux
 
 
 
 def reproduccion_parte_inicial():
     pygame.mixer.init()
-    pygame.mixer.music.load("grabaciones/parte1.mp3")
+    pygame.mixer.music.load("grabaciones/parte1.ogg")
     pygame.mixer.music.play()
 
 
 
-def reproduccion_opcion_inicial():
-    pygame.mixer.init()
-    pygame.mixer.music.load("opciones/opcion1.mp3")
-    pygame.mixer.music.play()
+#def reproduccion_opcion_inicial():
+ #   pygame.mixer.init()
+  #  pygame.mixer.music.load("opciones/opcion1.mp3")
+   # pygame.mixer.music.play()
 
 
 
 def main():
 
     reproduccion_parte_inicial()
-    reproduccion_opcion_inicial()
+    #reproduccion_opcion_inicial()
     parte_anterior = "parte1"
-    opcion_anterior= "opcion1"
+    #opcion_anterior= "opcion1"
 
 
     while True:
         keys = pygame.key.get_pressed()
-        opcion_anterior = reproduce_opcion(opcion_anterior , keys)
-        parte_anterior = reproduce_parte(parte_anterior , keys)
+        
+        #opcion_anterior = reproduce_opcion(opcion_anterior , keys)
 
-        if parte_anterior == "error" or opcion_anterior == "error":
-            sys.exit(0)
+        while(keys[K_a] or keys[K_b] or keys[K_c]):
+            parte_anterior = reproduce_parte(parte_anterior , keys)
+            print parte_anterior
+            print "------"+str(keys)
+
+            if parte_anterior == "error":
+                sys.exit(0)
 
     return 0
 
