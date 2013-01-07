2352c152a3b11a14ee1ae9c9163161f923bd9fdf
Isaac Torres <ismatorr@gmail.com>
2013-01-07 01:26:53 -0500

    codigo listo y comentado

--- Adventure/src/story.py
+++ Adventure/src/story.py
@@ -6,18 +6,18 @@ import pygame
 from pygame.locals import *
 
 
-
+# funcion encargada de determinar que parte de la hisoria el usuario decidio continuar
 def reproduce_parte(p_ant , keys):
     cont = 0
-    if keys == pygame.K_a:
-        r = "grabaciones/"+p_ant+"_1.ogg"
-        aux = p_ant+"_1"
+    if keys == pygame.K_a: #si aplasto la tecla a
+        r = "grabaciones/"+p_ant+"_1.ogg" #variable a reproducir
+        aux = p_ant+"_1" # variable que guarda la parte actual en reproduccion
 
-    elif keys == pygame.K_b:
+    elif keys == pygame.K_b: # si aplasto la tecla b
         r = "grabaciones/"+p_ant+"_2.ogg"
         aux = p_ant+"_2"
 
-    elif keys == pygame.K_c:
+    elif keys == pygame.K_c: #si aplasto la tecla c
         r = "grabaciones/"+p_ant+"_3.ogg"
         aux = p_ant+"_3"
 
@@ -25,9 +25,7 @@ def reproduce_parte(p_ant , keys):
         r = "ggggg"
         aux = "error"
 
-    #print str(keys)+"------"+str(pygame.K_a)
-    #print r
-    #pygame.mixer.init()
+    
 
 
     try:
@@ -43,26 +41,7 @@ def reproduce_parte(p_ant , keys):
 
 
     
-#def reproduce_opcion(o_ant , keys):
-#    if keys[K_a] or keys[K_A]:
-#        o = "opciones/"+o_ant+"_1.mp3"
-#        aux = o_ant+"_1"
-#    if keys[K_b] or keys[K_B]:
-#        o = "opciones/"+o_ant+"_2.mp3"
-#        aux = o_ant+"_2"
-#    if keys[K_c] or keys[K_C]:
-#        o = "opciones/"+o_ant+"_3.mp3"
-#        aux = o_ant+"_3"
 
-#    pygame.mixer.init()
-#    try:
-#        pygame.mixer.music.load(o)
-#        pygame.mixer.music.play()
-#    except:
-#        aux = "error"
-
-
-#    return aux
 
 
 
@@ -85,42 +64,42 @@ def reproduccion_mensaje_error():
 
 
 
-#def reproduccion_opcion_inicial():
- #   pygame.mixer.init()
-  #  pygame.mixer.music.load("opciones/opcion1.mp3")
-   # pygame.mixer.music.play()
 
 
 
+#funcion que se encarga de controlar el accionar del usuario a través de la historia
 def main():
     screen = pygame.display.set_mode((640,480))
+    # se ejecuta una pantalla para mantener el foco del programa, ya que facilmente el usuario puede dar clic sin querer en otro lado y no se capturan los eventos de teclado
     reproduccion_parte_inicial()
-    #reproduccion_opcion_inicial()
+    
     parte_anterior = "parte1"
-    #opcion_anterior= "opcion1"
+    #al inicio la parte anterior es la primera
 
     while True:
         eventos = pygame.event.get()
+        #obtengo todos los eventos que el usuario realice
         for evento in eventos:
-            if evento.type == pygame.KEYDOWN:
-                if evento.key == pygame.K_q:
+            if evento.type == pygame.KEYDOWN: #si el usuario aplasto una tecla
+                if evento.key == pygame.K_q or evento.key == K_ESCAPE: # si fue la q o escape
                     sys.exit(0)
 
                 if evento.key == pygame.K_a or evento.key == pygame.K_b or evento.key == pygame.K_c:
+                # la historia tiene un máximo de 3 caminos todas las veces
                     parte_anterior = reproduce_parte(parte_anterior , evento.key)
-                    print parte_anterior
-                    #print "------"+str(keys)
+                    # reproduzco y recupero la parte que se esta reproduciendo
                     if parte_anterior == "error":
+                    # si hubo algun error al reproducir la opcion escogida por el usuario
                         reproduccion_mensaje_error()
                         sys.exit(0)
 
-        pygame.display.flip()
+        pygame.display.flip() #muestra la pantalla todas las veces
 
 
-    return 0
+    return 0 # fin del programa
 
 
 
 if __name__ == "__main__":
-    pygame.init()
+    pygame.init() #inicializa pygame
     main()
