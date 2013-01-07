2fd23aa0a22989c232502abc214539ce4deb93dd
Isaac Torres <ismatorr@gmail.com>
2013-01-07 00:59:05 -0500

    Codigo terminado y probado, solo faltaria agregar las grabaciones reales

--- Adventure/src/story.py
+++ Adventure/src/story.py
@@ -13,11 +13,11 @@ def reproduce_parte(p_ant , keys):
         r = "grabaciones/"+p_ant+"_1.ogg"
         aux = p_ant+"_1"
 
-    if keys == pygame.K_b:
+    elif keys == pygame.K_b:
         r = "grabaciones/"+p_ant+"_2.ogg"
         aux = p_ant+"_2"
-        
-    if keys == pygame.K_c:
+
+    elif keys == pygame.K_c:
         r = "grabaciones/"+p_ant+"_3.ogg"
         aux = p_ant+"_3"
 
@@ -25,7 +25,9 @@ def reproduce_parte(p_ant , keys):
         r = "ggggg"
         aux = "error"
 
-    pygame.mixer.init()
+    #print str(keys)+"------"+str(pygame.K_a)
+    #print r
+    #pygame.mixer.init()
 
 
     try:
@@ -73,6 +75,15 @@ def reproduccion_parte_inicial():
         cont = cont+1
 
 
+def reproduccion_mensaje_error():
+    cont = 0
+    pygame.mixer.init()
+    pygame.mixer.music.load("grabaciones/mensaje_error.ogg")
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy() == True:
+        cont = cont+1
+
+
 
 #def reproduccion_opcion_inicial():
  #   pygame.mixer.init()
@@ -82,23 +93,28 @@ def reproduccion_parte_inicial():
 
 
 def main():
-
+    screen = pygame.display.set_mode((640,480))
     reproduccion_parte_inicial()
     #reproduccion_opcion_inicial()
     parte_anterior = "parte1"
     #opcion_anterior= "opcion1"
 
-
     while True:
         eventos = pygame.event.get()
         for evento in eventos:
             if evento.type == pygame.KEYDOWN:
-                parte_anterior = reproduce_parte(parte_anterior , evento.key)
-                print parte_anterior
-                #print "------"+str(keys)
-                if parte_anterior == "error":
-                    #deberia ir un mensaje de eror
+                if evento.key == pygame.K_q:
                     sys.exit(0)
+
+                if evento.key == pygame.K_a or evento.key == pygame.K_b or evento.key == pygame.K_c:
+                    parte_anterior = reproduce_parte(parte_anterior , evento.key)
+                    print parte_anterior
+                    #print "------"+str(keys)
+                    if parte_anterior == "error":
+                        reproduccion_mensaje_error()
+                        sys.exit(0)
+
+        pygame.display.flip()
 
 
     return 0
